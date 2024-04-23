f3c2ebc4472e12c29abb15f656b089e18066e270
GollisUni <167691696+GollisUni@users.noreply.github.com>
2024-04-23 01:10:30 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+let admin=document.querySelector("#admin")
+let login=document.querySelector("#login")
+let choose=document.querySelector(".choose")
+
+
+if(choose.value="admin"){
+    login.style="display:none";
+}
+
+// if(choose.value="Students"){
+//     admin.style="display:none";
+// }
+
